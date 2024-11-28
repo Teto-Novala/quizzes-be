ece6c6e413114e89823e6ec93c778a5ee89847b6
Teto-Novala <tetocoding@gmail.com>
2024-11-28 11:44:52 +0700

    feat: set db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRE_HOST,
+      port: parseInt(<string>process.env.POSTGRE_PORT),
+      username: process.env.POSTGRE_USER,
+      password: process.env.POSTGRE_PASSWORD,
+      database: process.env.POSTGRE_DB,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
